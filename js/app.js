f6c43741e064a430005b1505f7accca6225b66a1
Adrian Islas <adrian.islas.lopez@gmail.com>
2024-01-15 21:42:49 -0600

    guardado de apis y diseño

---
+++ js/app.js
@@ -0,0 +1,57 @@
+  /*****************************Consulta api************************************* */
+const apiUrl = 'https://randomuser.me/api/?results=50&gender=female';
+
+let datosObtenidos = [];
+fetch(apiUrl)
+.then(response => {
+    if (!response.ok) {
+    throw new Error(`No se pudo vincular`);
+    }
+    return response.json();
+})
+.then(data => {
+    datosObtenidos = data;
+    let datos = Object.values(datosObtenidos.results);
+    
+    for( dato of datos){
+        let nombre = dato.name.first;
+        let apellido = dato.name.last;
+        let foto = dato.picture.large;
+        let email = dato.email;
+        let numero = dato.cell;
+
+        let contenedorUsuarios = document.querySelector(".ul-usuarios");
+
+        let liUsuario = document.createElement("li");
+
+        let h3Nombre = document.createElement("h3") ;
+        h3Nombre.textContent = `${nombre} ${apellido}`;
+
+        let imgUsuario = document.createElement("img");
+        imgUsuario.setAttribute("src", foto);
+        
+        let emailUsuario = document.createElement("span");
+        emailUsuario.classList.add("email-span");
+        emailUsuario.textContent = email;
+
+        let numeroUsuario = document.createElement("span");
+        numeroUsuario.classList.add("numero-usuario");
+        numeroUsuario.textContent=numero;
+
+
+        contenedorUsuarios.appendChild(liUsuario);
+        liUsuario.appendChild(imgUsuario);
+        liUsuario.appendChild(h3Nombre);
+        liUsuario.appendChild(numeroUsuario);
+        liUsuario.appendChild(emailUsuario);
+        
+
+    }
+
+
+})
+.catch(error => {
+    console.error('Error al realizar la solicitud:', error);
+});
+  
+
---
+++ js/slider.js
@@ -0,0 +1,11 @@
+        /************************slider****************************************** */
+        var swiper = new Swiper(".mySwiper", {
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+          autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+          },
+        });
